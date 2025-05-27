6ecee2dcc98a085f4847625fe1dfaa8e25a53cd4
Dell <Dell@DESKTOP-DQHMPDK>
2025-05-27 23:05:09 +0700

    duyen

--- src/main/java/com/example/qiuzweb/controller/user/Homecontroller.java
+++ src/main/java/com/example/qiuzweb/controller/user/Homecontroller.java
@@ -1,8 +1,47 @@
 package com.example.qiuzweb.controller.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Homecontroller {
-    
+
+    @GetMapping("/Login")
+    public String loginPage() {
+        return "user/Login";
+    }
+
+    @GetMapping("/Signup")
+    public String signupPage() {
+        return "user/Signup";
+    }
+
+    @GetMapping("/Password")
+    public String passwordPage() {
+        return "user/Password";
+    }
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
+
+    @GetMapping("/accounts")
+    public String accounts() {
+        return "account-management";
+    }
+
+    @GetMapping("/quizzes")
+    public String quizzes() {
+        return "quiz-management";
+    }
+
+    @GetMapping("/quizzes/create")
+    public String createQuiz() {
+        return "create-account";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "redirect:/login";
+    }
 }
